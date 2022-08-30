e597cdbaad70a8dadfb7819cc43aebf0ab6e66d9
André Luiz dos Santos <69798730+andreluizdsantos@users.noreply.github.com>
2022-08-30 15:37:19 -0300

    Update InstallPortainer.sh

--- InstallPortainer.sh
+++ InstallPortainer.sh
@@ -2,5 +2,5 @@
 echo "Criando volume Portainer no Docker"
 docker volume create portainer_data
 echo "Deploy Portainer"
-docker run -d -p 9000:9000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+docker run -d -p 9000:9000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
 echo "Acesse http://localhost:9000"
